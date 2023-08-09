1f56f61d005ee82097d857bee9639751a7805816
Adrian Galvan <jaggalvan@hotmail.com>
2023-08-09 11:58:09 -0600

    feat: add dynamic form

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -38,7 +38,7 @@ export default function Services() {
                     <h2 className="text-center">Services</h2>
                     <h4 className="pb-3 text-center">Check some of my services that I did</h4>
 
-                  <Row xs={12} md={3} className="g-4 mt-4">
+                  <Row xs={1} md={3} className="g-4 mt-4">
                     {serviceArray.map( (service,index)=>(
                         <CardService service={service} key={index}/>
 
